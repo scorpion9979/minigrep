475d423585db13510e93e36f0bfa85db27d60522
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2019-01-19 19:42:04 +0300

    Re-implemented search methods with iterator adapters

--- src/lib.rs
+++ src/lib.rs
@@ -39,23 +39,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
+    let results = contents.lines().filter(|line| {
+        line.contains(&query)
+    }).collect();
     results
 }
 
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
+    let results = contents.lines().filter(|line| {
+        line.to_lowercase().contains(&query)
+    }).collect();
     results
 }
 
